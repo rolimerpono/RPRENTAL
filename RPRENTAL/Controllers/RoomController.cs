bde016672839da3dd9a66dd2a75743e7439be966
Rolimer Pono <rolimer_pono@yahoo.com>
2024-03-20 10:59:35 +1300

    Add Edit Delete

--- RPRENTAL/Controllers/RoomController.cs
+++ RPRENTAL/Controllers/RoomController.cs
@@ -40,6 +40,43 @@ namespace RPRENTAL.Controllers
         }
 
         [HttpGet]
+        public IActionResult Create()
+        {
+            Room objRoom;
+            try
+            {
+                objRoom = new Room { ROOM_ID = 0, DESCRIPTION = "", ROOM_NAME = "", ROOM_PRICE = 0, MAX_OCCUPANCY = 0, IMAGE_URL = "https://placehold.co/600x400", CREATED_DATE = DateTime.Now };
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+            return PartialView("Create", objRoom);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Room objRoom)
+        {
+            try
+            {
+                if (ModelState.IsValid && objRoom.ROOM_ID == 0)
+                {
+                    _IRoomService.Create(objRoom);
+                    return Json(new { success = true, message = "Room created successfully." });
+                }
+                return Json(new { success = false, message = "Something went wrong." });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+
+        }
+
+        [HttpGet]
         public IActionResult Update(int ROOM_ID)
         {
             try
@@ -63,44 +100,28 @@ namespace RPRENTAL.Controllers
                 if (ModelState.IsValid && objRoom.ROOM_ID > 0)
                 {
                     _IRoomService.Update(objRoom);
-                }            
-            }catch (Exception ex) { 
-            
-            }
-           return RedirectToAction("Index");
-        
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            try
-            {
-                Room objRoom =new Room{ ROOM_ID = 0, DESCRIPTION="", ROOM_NAME ="", ROOM_PRICE = 0, MAX_OCCUPANCY = 0, IMAGE_URL = "https://placehold.co/600x400", CREATED_DATE = DateTime.Now };             
-                return PartialView("Create", objRoom);
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-
-        }
-
-        [HttpPost]
-        public IActionResult Create(Room objRoom)
-        {
-            try
-            {
-                if (ModelState.IsValid && objRoom.ROOM_ID == 0)
-                {
-                    _IRoomService.Create(objRoom);                   
                 }
             }
             catch (Exception ex)
             {
-
+                return Json(new { success = false, message = ex.Message });
             }
-            return RedirectToAction("Index");
+            return RedirectToAction("Index");        
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int ROOM_ID)
+        {
+            try
+            {            
+               
+                _IRoomService.Delete(ROOM_ID);
+                return Json(new { success = true, message = "Room deleted successfully." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
     }
 }
--- RPRENTAL/wwwroot/js/Crud/JSRoom/JSRoomCrud.js
+++ RPRENTAL/wwwroot/js/Crud/JSRoom/JSRoomCrud.js
@@ -17,59 +17,107 @@ document.addEventListener("DOMContentLoaded", function () {
                 { data: 'maX_OCCUPANCY', "width": "5%" },
                 { data: 'imagE_URL', "width": "5%" },
                 {
-                    data: null,
+                    data: 'rooM_ID',
                     "width": "5%",
                     "render": function (data, type, row) {
-                        return '<button class="btn btn-primary btn-sm select-row w-100">Edit</button>';
+                        return '<button class="btn btn-primary btn-sm select-edit-btn w-100">Edit</button>';
                     }
                 },
                 {
-                    data: null,
+                    data: 'rooM_ID',
                     "width": "5%",
                     "render": function (data, type, row) {
-                        return '<button class="btn btn-danger btn-sm delete-btn w-100">Delete</button>';
+                        return '<button class="btn btn-danger btn-sm select-delete-btn w-100">Delete</button>';
                     }
                 }
 
             ],
             "columnDefs":
                 [
-                    { "className": "dt-left", "targets": "_all" },
+                    { "className": "dt-right", "targets": "_all" },
                     {
                         "targets": [2], "className": "dt-nowrap", "render": function (data, type, row) {
                             return type === 'display' && data.length > 100 ? // Adjust the threshold as needed
                                 '<span title="' + data + '">' + data.substr(0, 100) + '...</span>' : data;
                         }
-                    } // Hide extra text in the 'description' and 'imagE_URL' columns
+                    } // Hide extra text in the 'description'
                 ],
             fixedColumns: true,
             scrollY: true
 
 
         });
- 
 
-    //EDIT
-    $("#tbl_Rooms").on("click", ".select-row", function () {
+
+    //SELECT ROW BTN DELETE CLICK
+    $("#tbl_Rooms").on("click", ".select-delete-btn", function () {
         var rowData = objDataTable.row($(this).closest('tr')).data();
 
-        var data = { ROOM_ID: rowData.rooM_ID };
+        console.log("DELETE BUTTON CLICK");
+      
+
+    });
+ 
+
+    ////SELECT ROW BTN EDIT CLICK
+    //$("#tbl_Rooms").on("click", ".select-edit-btn", function () {
+    //    var rowData = objDataTable.row($(this).closest('tr')).data();
+
+    //    var data = { ROOM_ID: rowData.rooM_ID };
+    //    $.ajax(
+    //        {
+    //            type: "GET",
+    //            url: "/Room/Update",
+    //            contentType: "application/json; charset=utf=8",
+    //            data: data,
+    //            success: function (result) {                  
+    //                $("#modal-edit-content").html(result);
+    //                $("#modal-edit").modal("show");
+    //            },
+    //            error: function (xhr, status, error) {
+    //                console.log("Error: " + error);
+    //            }   
+    //        });         
+   
+    //});
+
+    $(".btn-add").click(function () {
+
         $.ajax(
             {
                 type: "GET",
-                url: "/Room/Update",
+                url: "/Room/Create",
                 contentType: "application/json; charset=utf=8",
-                data: data,
                 success: function (result) {
                     console.log(result);
-                    $("#modal-edit-content").html(result);
-                    $("#modal-edit").modal("show");
+                    $("#modal-add-content").html(result);
+                    $("#modal-add").modal("show");
                 },
                 error: function (xhr, status, error) {
                     console.log("Error: " + error);
-                }   
-            });         
-   
+                }
+            });
+    });
+
+
+    $(".btn-save").click(function () {
+
+        var objRoomData = $("#saveform").serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "/Room/Create",
+            data: objRoomData,
+            success: function (response) {
+                objDataTable.ajax.reload();
+                $("#modal-add").modal("hide");
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+
+        })
+
     });
 
 
@@ -94,49 +142,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     //END EDIT
 
-    $(".btn-add").click(function () {
 
-        $.ajax(
-            {
-                type: "GET",
-                url: "/Room/Create",
-                contentType: "application/json; charset=utf=8",              
-                success: function (result) {
-                    console.log(result);
-                    $("#modal-add-content").html(result);
-                    $("#modal-add").modal("show");
-                },
-                error: function (xhr, status, error) {
-                    console.log("Error: " + error);
-                }
-            });    
-    });
+   
 
-
-
-
-
-    $(".btn-save").click(function () {
-
-        var objRoomData = $("#saveform").serialize();
-
-        $.ajax({
-            type: "POST",
-            url: "/Room/Create",
-            data: objRoomData,
-            success: function (response) {
-                objDataTable.ajax.reload();
-                $("#modal-add").modal("hide");
-            },
-            error: function (xhr, status, error) {
-                console.log("Error: " + error);
-            }
-
-        })
-
-    });
-
- 
        
 });
 
