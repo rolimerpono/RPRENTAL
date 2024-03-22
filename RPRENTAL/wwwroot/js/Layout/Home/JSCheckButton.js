8c58249107485cfe46c23a3c99fdc8912464267e
Rolimer Pono <rolimer_pono@yahoo.com>
2024-03-22 21:54:44 +1300

    Fixed Pagination no page refresh

---
+++ RPRENTAL/wwwroot/js/Layout/Home/JSCheckButton.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    // Check if btnSoldOut or btnBookNow is visible
+    var isBtnSoldOutVisible = $('#btnSoldOut').is(':visible');
+    var isBtnBookNowVisible = $('#btnBookNow').is(':visible');
+
+    // If either btnSoldOut or btnBookNow is visible, update btnBookNow action and route parameters
+    if (isBtnSoldOutVisible || isBtnBookNowVisible) {
+        $('#btnBookNow').attr('asp-action', 'GetRoomAvailable');       
+    }
+});
--- RPRENTAL/wwwroot/js/Layout/Home/JSRoomAvailable.js
+++ RPRENTAL/wwwroot/js/Layout/Home/JSRoomAvailable.js
@@ -1,12 +1,15 @@
-﻿$(document).ready(function () {    
+﻿$(document).ready(function () {
 
-        const btnCheck = document.getElementById('btnCheck');debugger
+    const btnCheck = document.getElementById('btnCheck'); 
 
-        btnCheck.addEventListener('click', function () {
+    btnCheck.addEventListener('click', function () {
+        
 
-            const checkInDate = document.getElementById('CheckInDate').value;
-            const checkOutDate = document.getElementById('CheckOutDate').value;
-            const roomListDiv = document.getElementById('ROOM_LIST');
+        const checkInDate = document.getElementById('CheckInDate').value;
+        const checkOutDate = document.getElementById('CheckOutDate').value;
+        const roomListDiv = document.getElementById('room_list');
+
+       
 
 
             const url = '/Home/GetRoomAvailable';
@@ -14,6 +17,8 @@
             body.append('CHECKIN_DATE', checkInDate);
             body.append('CHECKOUT_DATE', checkOutDate);
 
+        if (checkOutDate >= checkInDate) {
+            
 
             fetch(url, {
                 method: 'POST',
@@ -38,6 +43,58 @@
                 .catch(error => {
                     console.error('There was a problem with the fetch operation:', error);
                 });
-        });
+        }
+
+    });
+
+
+    
+   
 
 });
+
+function GetRoomAvailable(pageID) {    
+    const checkInDate = document.getElementById('CheckInDate').value;
+    const checkOutDate = document.getElementById('CheckOutDate').value;
+    const roomListDiv = document.getElementById('room_list');
+    
+
+
+
+
+    const url = '/Home/GetRoomAvailable';
+    const body = new URLSearchParams();
+    body.append('CHECKIN_DATE', checkInDate);
+    body.append('CHECKOUT_DATE', checkOutDate);
+    body.append('iPage', pageID);
+
+    if (checkOutDate >= checkInDate) {
+
+
+        fetch(url, {
+            method: 'POST',
+            body: body,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                }
+            }).then(data => {
+                console.log(data);
+
+                roomListDiv.innerHTML = '';
+                const responseDataDiv = document.createElement('div');
+                responseDataDiv.innerHTML = data;
+                roomListDiv.appendChild(responseDataDiv);
+
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+};
+
