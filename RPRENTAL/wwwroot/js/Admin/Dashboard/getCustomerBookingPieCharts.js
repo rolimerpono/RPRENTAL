c564f0051da5509640d2aebf2335d6a92cfedb63
rolimer <rolimer_pono@yahoo.com>
2024-04-28 21:33:10 +1200

    Admin Layout

--- RPRENTAL/wwwroot/js/Admin/Dashboard/getCustomerBookingPieCharts.js
+++ RPRENTAL/wwwroot/js/Admin/Dashboard/getCustomerBookingPieCharts.js
@@ -10,29 +10,42 @@ function LoadCutomerBookings() {
         type: 'Get',
         dataType: 'json',
         success: function (data) {
-            loadPieChart("customer_increase_decrease_status", data);
+            loadCustomerPieChart("customer_increase_decrease_status", data);
+          
         }
     });
 }
 
-function loadPieChart(id, data) {
+function loadCustomerPieChart(id, data) {   
+
+
     let chartColors = getChartColor(id);
 
     var options = {
         series: data.series,
         chart: {
-            height: 480,
+            height: 160,
             type: 'pie',
         },
-        labels: data.series,
-        legend: {
-            position: 'bottom',
-            fontSize: '24px',
-            fontWeight: 600,
-        },
+        labels: data.labels,    
         colors: chartColors,
+        legend: {
+            show: false,
+            showForSingleSeries: false,
+            showForNullSeries: true,
+            showForZeroSeries: true,
+            position: 'bottom',
+            horizontalAlign: 'center',      
+            offsetY: 10,
+
+            itemMargin: {
+                horizontal: 15,
+                vertical: 0
+            }       
+           
+        },
         responsive: [{
-            breakpoint: 480,
+            breakpoint: 190,
             options: {
                 chart: {
                     width: 200
