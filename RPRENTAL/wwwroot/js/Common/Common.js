56b059c4908e414f49f7641a5db27b2622941d7f
rolimer <rolimer_pono@yahoo.com>
2024-05-04 11:02:55 +1200

    Text Truncated

--- RPRENTAL/wwwroot/js/Common/Common.js
+++ RPRENTAL/wwwroot/js/Common/Common.js
@@ -99,18 +99,3 @@ function GetRowData(objDataTable, btn) {
 
 
 
-
-
-$(document).ready(function () {
-
-	let span = document.getElementById('#description-span');
-
-	if (span.scrollHeight > 70) {
-		span.style.display = '-webkit-box';
-		span.style.webkitBoxOrient = 'vertical';
-		span.style.webkitLineClamp = '100'; // Number of lines to show before truncating
-		span.style.overflow = 'hidden';
-	}
-
-
-});
